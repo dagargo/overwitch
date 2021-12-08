dagargo/overwitch

commit 1f1a69597d555b2b5007d085deb30bc5f3d30978

Terminate gracefully if a new transfer can not be submitted as the device may have been disconnected

src/overbridge.c
@@ -480,6 +480,7 @@ prepare_cycle_out (struct overbridge *ob)
     {
       error_print ("j2o: Error when submitting USB audio transfer: %s\n",
 		   libusb_strerror (err));
+      overbridge_set_status (ob, OB_STATUS_ERROR);
     }
 }
 
@@ -495,6 +496,7 @@ prepare_cycle_in (struct overbridge *ob)
     {
       error_print ("o2j: Error when submitting USB audio in transfer: %s\n",
 		   libusb_strerror (err));
+      overbridge_set_status (ob, OB_STATUS_ERROR);
     }
 }
 
@@ -510,6 +512,7 @@ prepare_cycle_in_midi (struct overbridge *ob)
     {
       error_print ("o2j: Error when submitting USB MIDI transfer: %s\n",
 		   libusb_strerror (err));
+      overbridge_set_status (ob, OB_STATUS_ERROR);
     }
 }
 
@@ -525,6 +528,7 @@ prepare_cycle_out_midi (struct overbridge *ob)
     {
       error_print ("j2o: Error when submitting USB MIDI transfer: %s\n",
 		   libusb_strerror (err));
+      overbridge_set_status (ob, OB_STATUS_ERROR);
     }
 }
 
@@ -871,7 +875,7 @@ run_audio_and_o2j_midi (void *data)
 	  libusb_handle_events_completed (ob->context, NULL);
 	}
 
-      if (overbridge_get_status (ob) == OB_STATUS_STOP)
+      if (overbridge_get_status (ob) <= OB_STATUS_STOP)
 	{
 	  break;
 	}

src/overbridge.h
@@ -182,6 +182,7 @@ typedef enum
 
 typedef enum
 {
+  OB_STATUS_ERROR,
   OB_STATUS_STOP,
   OB_STATUS_READY,
   OB_STATUS_BOOT_OVERBRIDGE,

commit 9b1bbd80b5551e889e9552e0ee61a9007c163258

Fix regression (SIGINT unresponsive) introduced in 1f1a6959

src/overbridge.c
@@ -831,7 +831,7 @@ run_j2o_midi (void *data)
 	}
       nanosleep (&req, NULL);
 
-      if (!overbridge_get_status (ob))
+      if (overbridge_get_status (ob) <= OB_STATUS_STOP)
 	{
 	  break;
 	}